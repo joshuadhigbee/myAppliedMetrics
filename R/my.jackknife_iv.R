e999eaabde7f8a6d3819f647180de2e21e602493
Joshua Higbee <63010923+joshuadhigbee@users.noreply.github.com>
2020-11-17 23:38:17 -0600

    Add Jackknife, edit TSLS docs

--- R/my.jackknife_iv.R
+++ R/my.jackknife_iv.R
@@ -1,4 +1,4 @@
-#' Two-Stage Least Squares
+#' Jackknife IV Estimator
 #'
 #' This function returns point estimates and the variance-covariance matrix.
 #' Written for data.table arguments.
@@ -13,14 +13,14 @@
 #' @keywords ols linear
 #' @export
 #' @examples
-#' my.tsls()
+#' my.jackknife_iv()
 
-my.tsls <- function(Yvar, Xvar, Inc, Exc, data, intercept=TRUE,
+my.jackknife_iv <- function(Yvar, Xvar, Inc, Exc, data, intercept=TRUE,
                    varcov="het", tolerance=1e-16) {
   # Select data and add intercept
   Y <- data[,..Yvar]
-  if (Inc != "") {
-    X <- data[,c(..Xvar,..Inc)] # Must be char vector
+  if (Inc!="") { # If there are included instruments besides intercept
+    X <- data[,c(..Inc, ..Xvar)] # Must be char vector
     Z <- data[,c(..Inc, ..Exc)] # Must be char vectors
   } else {
     X <- data[,..Xvar] # Must be char vector
@@ -33,26 +33,31 @@ my.tsls <- function(Yvar, Xvar, Inc, Exc, data, intercept=TRUE,
     setcolorder(Z, c(".intercept"))
   }
 
+  # Construct jackknife instrument
+  ZtZinv <- solve(t(Z) %*% as.matrix(Z), tol=tolerance)
+  h_i <- diag(as.matrix(Z) %*% ZtZinv %*% t(Z))
+  Pi_hat <- t(X) %*% as.matrix(Z) %*% ZtZinv
+  X_jack <- (1-h_i)^(-1)*(as.matrix(Z) %*% t(Pi_hat)  - h_i * X)
+
   # Coefficients
-  P_Z <- as.matrix(Z) %*% solve(t(Z) %*% as.matrix(Z), tol=tolerance) %*% t(Z)
-  XtXinv_proj <- solve(t(X) %*% P_Z %*% as.matrix(X), tol=tolerance)
-  XtY_proj <- t(X) %*% P_Z %*% as.matrix(Y)
-  beta <- XtXinv_proj %*% XtY_proj
+  XtXinv_jack <- solve(t(X_jack) %*% as.matrix(X), tol=tolerance)
+  XtY_jack <- t(X_jack) %*% as.matrix(Y)
+  beta <- XtXinv_jack %*% XtY_jack
   colnames(beta) <- c("coefs")
 
   # Predicted values, residuals, and squared residuals
-  Xhat <- P_Z %*% as.matrix(X)
   Yhat <- as.matrix(X) %*% as.matrix(unlist(beta))
   U <- as.matrix(as.matrix(Y) - Yhat)
   U2 <- c(U) * c(U)
 
   # Variances (multiple options)
   if (varcov == "het") {
-    Sigma <- XtXinv_proj %*% (t(Xhat) %*% diag(U2) %*% as.matrix(Xhat)) %*%
-      XtXinv_proj * nrow(X)/(nrow(X)-ncol(X))
+    Sigma <- XtXinv_jack %*% (t(X_jack) %*% diag(U2) %*% as.matrix(X_jack)) %*%
+      t(XtXinv_jack) * nrow(X)/(nrow(X)-ncol(X))
   }
   if (varcov == "hom") {
-    Sigma <- sum(U2)/(nrow(X)-ncol(X)) * XtXinv_proj
+    Sigma <- sum(U2)/(nrow(X)-ncol(X)) * XtXinv_jack %*%
+      (t(X_jack) %*% as.matrix(X_jack)) %*% t(XtXinv_jack)
   }
 
   # Return results
